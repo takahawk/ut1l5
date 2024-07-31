419e8c71b89af0a097ea62040568a379e2ebd341
w <ghostoftheshell@mailbox.org>
2024-07-31 15:58:50 +0300

    Add CLI coloring macros

--- io.h
+++ io.h
@@ -12,10 +12,25 @@
 #define CLI_COLOR_WHITE   "\x1B[37m"
 #define CLI_COLOR_RESET   "\x1B[0m"
 
+#define _uIO_RedChar(X) CLI_COLOR_RED X CLI_COLOR_RESET
+#define _uIO_RedString(X) _Generic(X, String: _uIO_MakeRed, default: __uIO_Nop) (X)
+#define uIO_Red(X) _Generic(X, \
+	String: _uIO_RedString(X), \
+	char*: _uIO_RedChar(X)  \
+	)
+
+#define _uIO_GreenChar(X) CLI_COLOR_RED X CLI_COLOR_RESET
+#define _uIO_GreenString(X) _Generic(X, String: _uIO_MakeGreen, default: __uIO_Nop) (X)
+#define uIO_Green(X) _Generic(X, \
+	String: _uIO_GreenString(X), \
+	char*: _uIO_GreenChar(X)  \
+	)
+
+static inline void __uIO_Nop(void *) {}
 
 // original string will be consumed
 static inline String
-uIO_MakeRed(String string) {
+_uIO_MakeRed(String string) {
 	String result = S_Format("%s%s%s", CLI_COLOR_RED, string.str, CLI_COLOR_RESET);
 	S_Free(&string);	
 	return result;
@@ -23,7 +38,7 @@ uIO_MakeRed(String string) {
 
 // original string will be consumed
 static inline String
-uIO_MakeGreen(String string) {
+_uIO_MakeGreen(String string) {
 	String result = S_Format("%s%s%s", CLI_COLOR_GREEN, string.str, CLI_COLOR_RESET);
 	S_Free(&string);	
 	return result;
